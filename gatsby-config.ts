af475a3927b1a9e76a28f0273a6c552cebfcbcaa
rajat <naiktravelsinfo@gmail.com>
2024-07-30 21:28:44 +0530

    Added workflow for github pages auto deployment.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,14 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `naiktravelservice`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.naiktravels.github.io`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss"],
+  pathPrefix: "/naiktravels.github.io", // Add this line
 };
 
 export default config;
